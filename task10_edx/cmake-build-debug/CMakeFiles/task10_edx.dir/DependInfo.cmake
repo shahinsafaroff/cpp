95f3995a9ce2749241a5200f282bedd84d93fe17
Shahin Safaroff <shahinsafaroff@gmail.com>
2021-04-07 01:11:20 +0400

    LeapYear is fixed

--- task10_edx/cmake-build-debug/CMakeFiles/task10_edx.dir/DependInfo.cmake
+++ task10_edx/cmake-build-debug/CMakeFiles/task10_edx.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/shahins/CLionProjects/task10_edx/main.cpp" "C:/Users/shahins/CLionProjects/task10_edx/cmake-build-debug/CMakeFiles/task10_edx.dir/main.cpp.obj"
+  "C:/Users/shahins/CLionProjects/c++/task10_edx/main.cpp" "C:/Users/shahins/CLionProjects/c++/task10_edx/cmake-build-debug/CMakeFiles/task10_edx.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- task10_edx/main.cpp
+++ task10_edx/main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 int year;
-bool leap;
 
-bool isleapyear(int year);
+bool checkYear(int inyear);
 
-int main() {
+int main()
+{
+    cin>>year;
 
-    cout << "Please enter year: " << endl;
-    cin >> year;
-    bool isleapyear( int year);
+    checkYear(year) ? cout << "Leap Year":
+    cout << "Not a Leap Year";
     return 0;
 }
-bool isleapyear (int year) {
-    leap = false;
-    if (year % 4 == 0) {
-        leap = true;
-        if (year % 100 == 0) {
-            leap = false;
-            if (year % 400 == 0) {
-                leap = true;
-           }
-        }
-     }
-    return leap;
+
+bool checkYear(int inyear)
+{
+    if (inyear % 400 == 0)
+        return true;
+
+    if (inyear % 100 == 0)
+        return false;
+
+    if (inyear % 4 == 0)
+        return true;
+    return false;
 }
