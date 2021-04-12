4859b65baf399d342f9499a27407cfdb518db8fe
shahinsafaroff <52258958+shahinsafaroff@users.noreply.github.com>
2021-04-12 21:55:15 +0400

    Update callcoster.cpp

--- task17_edx/callcoster.cpp
+++ task17_edx/callcoster.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <iomanip>
 using namespace std;
 int main () {
     int minutes, startTime;
@@ -7,49 +7,29 @@ int main () {
     float cost, rate;
     cout << "Enter the day the call started at: ";
     cin >> day;
-    if(day == "Mon"||day == "Tue" || day =="Wed"|| day =="Thu"|| day == "Fri") {
-        cout << "Enter the time the call started at (hhmm): ";
-        cin >> startTime;
-        if (startTime < 0 || startTime >= 2400) {
-            cout << "Invalid time."<<endl;
-            cout << "Enter start time of the call(For example, 1:30 = 1330): ";
-        }
-        else if (startTime >= 800 && startTime <= 1800) {
+    cin >> startTime;
+    cin >> minutes;
+    if (minutes < 0 || startTime < 0 || startTime >= 2400) {
+        cout << "Invalid time." << endl;
+        cout << "The number of minutes must be input as a positive integer";
+        cout << "Enter start time of the call(For example, 1:30 = 1330): ";
+    }
+    else if(day == "Mon"||day == "Tue" || day =="Wed"|| day =="Thu"|| day == "Fri") {
+        if (startTime >= 800 && startTime <= 1800) {
             rate = 0.4;
-            cout << "Enter the duration of the call (in minutes): ";
-            cin >> minutes;
-            if (minutes < 0) {
-                cout << "Invalid time."<<endl;
-                cout << "The number of minutes must be input as a positive integer";
-                cin >> minutes;
-            }
             cost = float(minutes) * rate;
-            cout << "This call will cost $" << cost << endl;
+            cout <<"This call will cost $" <<fixed<< setprecision(2)<< cost << endl;
         }
         else if (startTime < 800 || startTime > 1800){
             rate = 0.25;
-            cout << "Enter the duration of the call (in minutes): ";
-            cin >> minutes;
-            if (minutes < 0) {
-                cout << "Invalid time."<<endl;
-                cout << "The number of minutes must be input as a positive integer";
-                cin >> minutes;
-            }
             cost = float(minutes) * rate;
-            cout << "This call will cost $" << cost << endl;
+            cout << "This call will cost $" <<fixed<< setprecision(2)<< cost << endl;
         }
     }
     else if(day =="Sat" || day =="Sun"){
         rate = 0.15;
-        cout << "Enter the duration of the call (in minutes): ";
-        cin >> minutes;
-        if (minutes < 0) {
-            cout << "Invalid time."<<endl;
-            cout << "The number of minutes must be input as a positive integer";
-            cin >> minutes;
-        }
         cost = float(minutes) * rate;
-        cout << "This call will cost $" << cost << endl;
+        cout <<"This call will cost $" <<fixed<< setprecision(2)<< cost << endl;
     }
     else {
         cout << "Invalid."<<endl;
