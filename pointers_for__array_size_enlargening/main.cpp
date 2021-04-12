8dd8fc6311f303b29135264562c53f55b12c6907
Shahin Safaroff <shahinsafaroff@gmail.com>
2021-04-12 21:50:02 +0400

    pointers added

--- pointers_for__array_size_enlargening/main.cpp
+++ pointers_for__array_size_enlargening/main.cpp
@@ -5,9 +5,16 @@ void resizeArray(int *&array, int currentSize, int newSize){
     int *temp = new int [newSize];
     for (int i = 0; i < currentSize; ++i){
         temp[i] = array[i];
+        cout<<array[i]<<" ";
     }
+    cout<<endl;
     delete [] array;
     array = temp;
+    for (int i = 0; i < newSize; ++i){
+        temp[i] = array[i];
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
 }
 
 int main() {
@@ -19,8 +26,12 @@ int main() {
     int oneMore = 110;
     int newSize = size *2;
     resizeArray(array, size, newSize);
-    size = newSize;
-    array[size++] =oneMore;
-    cout<<oneMore<<endl;
+//    size = newSize;
+    array[size] = oneMore;
+//    cout<<oneMore<<endl;
+for(int i=0;i<newSize;i++){
+    cout<<array[i]<<" ";
+}
+cout<<endl;
     return 0;
 }
